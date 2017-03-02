10cd2f4c8444e5b24cc4c674ef7de3513b0c12a2
VMRS <simon.mehu@gmail.com>
2017-03-02 10:50:09 +0100

    change env

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,8 +5,8 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://toto:toto@ds151049.mlab.com:51049/vmrs'
-    //uri: 'mongodb://localhost/vmrs'
+    //uri: 'mongodb://toto:toto@ds151049.mlab.com:51049/vmrs'
+    uri: 'mongodb://localhost/vmrs'
 },
-  seedDB: true
+  seedDB: false
 };
