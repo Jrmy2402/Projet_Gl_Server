16ea1d9b438e5148c0a90f7a3ed9a6a283cbd299
Jérémy Spriet <jeremy.spriet@gmail.com>
2017-03-17 16:09:06 +0100

    aiojzj

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -134,7 +134,7 @@ exports.show = function (req, res, next) {
   User.findById(userId, function (err, user) {
     if (err) return next(err);
     if (!user) return res.status(401).send('Unauthorized');
-    res.json(user.profile);
+    res.json(user);
   });
 };
 
